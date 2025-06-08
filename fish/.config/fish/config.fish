00338ce0fb8894e81021dbc9e39df265489dbe67
AW-Koi <max.kooij@ahwoo.com>
2025-06-08 21:53:59 +1200

    fish ls aliases + starship ricing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,3 +8,15 @@ starship init fish | source
 if status is-interactive
     fastfetch
 end
+
+# Aliases
+# ls
+alias ls='eza'		           # basic
+alias ll='eza -l'		   # long format
+alias la='eza -la'		   # long format + hidden files
+alias lt='eza --tree'		   # tree
+alias lta='eza --tree -a'          # tree with hidden files  
+alias ltl='eza --tree -l'          # tree with long format
+alias ltla='eza --tree -la'        # tree with long format and hidden files
+alias lt2='eza --tree --level=2'   # tree limited to 2 levels deep
+alias lt3='eza --tree --level=3'   # tree limited to 3 levels deep
